a4a59fe9e13812a63a7a3017cfeec06702d53601
RohitGoyal24 <39431001+RohitGoyal24@users.noreply.github.com>
2018-12-16 15:46:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var i,n=5;
+console.log("Using for loop");
+for(i=1;i<=10;i++)
+{
+ var a=n*i;
+ console.log(a);
+}
+console.log("Using while loop");
+var i=1,n=5;
+while(i<=10)
+{
+var a=n*i;
+console.log(a);
+i++;
+}
